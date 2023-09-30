9547904b54e7a92965f2cce06e103a4788ff2667
alenjb <alenjb@naver.com>
2023-10-01 01:26:32 +0900

    [control_num 구현]
    - 번호를 추첨할 때 필요한 조건들을 만족하는지 확인하는 함수들 구현
    - 구현 함수들:
          1. 연속된 3개의 숫자가 있는지 확인하는 함수(has_consecutive_numbers)
          2. 10의 자리 수가 같은 요소가 3개 이상인지 확인하는 함수(has_same_three_tens)
          3. 이전 회차와 같은 숫자가 3개 이상 있는지 확인하는 함수(has_same)

---
+++ control_num.py
@@ -0,0 +1,38 @@
+# 연속된 3개의 숫자가 있는지 확인하는 함수
+
+
+def has_consecutive_numbers(lst):
+    lst.sort()
+    for i in range(len(lst) - 2):
+        if lst[i] == lst[i+1] - 1 == lst[i+2] - 2:
+            return True
+    return False
+
+
+# 10의 자리 수가 같은 요소가 3개 이상인지 확인하는 함수
+def has_same_three_tens(lst):
+    tens_dict = {}  # 10의 자리 수를 키로, 출현 횟수를 값으로 하는 딕셔너리
+
+    for number in lst:
+        tens_place = number // 10  # 10의 자리 수 계산
+
+        if tens_place in tens_dict:
+            tens_dict[tens_place] += 1
+        else:
+            tens_dict[tens_place] = 1
+
+    # 딕셔너리에서 값이 3 이상인 키가 있는지 확인
+    for count in tens_dict.values():
+        if count >= 3:
+            return True
+
+    return False
+
+
+# 이전 회차와 같은 숫자가 3개 이상 있는지 확인하는 함수
+def has_same(num, last_week, two_weeks_ago, last, two_ago):
+    if num in last_week:
+        last +=1
+    if num in two_weeks_ago:
+        two_ago +=1
+    return last, two_ago
