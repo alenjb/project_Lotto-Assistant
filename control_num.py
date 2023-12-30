08b6e8835eed1199ab2dd22907725ae3d89d7951
alenjb <alenjb@naver.com>
2023-12-30 18:24:48 +0900

    🎨fix(control_num):숫자가 3개 이상인지를 각 메서드에서 판단하도록 수정
    - 연속된 3개의 숫자가 있는지 확인하는 메서드와 10의 자리 수가 같은 요소가 3개 이상인지 확인하는 메서드에서 리스트의 길이가 3 미만이면 False를 리턴하도록 수정함.
    - 이전 회차와 같은 숫자가 3개 이상 있는지 확인하는 메서드의 이름을 알아듣기 쉽게 수정함.

--- control_num.py
+++ control_num.py
@@ -1,7 +1,7 @@
 # 연속된 3개의 숫자가 있는지 확인하는 함수
-
-
-def has_consecutive_numbers(lst):
+def has_consecutive_numbers_more_than_three(lst):
+    if len(lst) < 3:
+        return False
     lst.sort()
     for i in range(len(lst) - 2):
         if lst[i] == lst[i+1] - 1 == lst[i+2] - 2:
@@ -11,6 +11,8 @@ def has_consecutive_numbers(lst):
 
 # 10의 자리 수가 같은 요소가 3개 이상인지 확인하는 함수
 def has_same_three_tens(lst):
+    if len(lst) < 3:
+        return False
     tens_dict = {}  # 10의 자리 수를 키로, 출현 횟수를 값으로 하는 딕셔너리
 
     for number in lst:
@@ -30,7 +32,7 @@ def has_same_three_tens(lst):
 
 
 # 이전 회차와 같은 숫자가 3개 이상 있는지 확인하는 함수
-def has_same(num, last_week, two_weeks_ago, last, two_ago):
+def has_same_with_last_week(num, last_week, two_weeks_ago, last, two_ago):
     if num in last_week:
         last +=1
     if num in two_weeks_ago:
