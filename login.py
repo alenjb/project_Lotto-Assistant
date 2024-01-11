8a20eaca208614aec78cf226a0158c0de965d66e
alenjb <alenjb@naver.com>
2024-01-11 20:36:37 +0900

    ✨feat(login):동행복권 구매 페이지(iframe)에서 번호들 선택하는 기능 구현
    [보완사항]
    - 확인 버튼을 눌러 구매를 완료하는 기능 구현 필요

--- login.py
+++ login.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
 import time
 
 import account
@@ -27,18 +29,17 @@ try:
     driver.get("https://el.dhlottery.co.kr/game/TotalGame.jsp?LottoId=LO40")
 
     # 대기 시간을 10초로 설정하고 iframe이 나타날 때까지 기다립니다.
-    WebDriverWait(driver, 2).until(EC.frame_to_be_available_and_switch_to_it((By.ID, "ifrm_tab")))
+    WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.ID, "ifrm_tab")))
 
-    # iframe 내부에서 라벨 찾기 (for 속성 값이 "check645num23"인 경우)
-    label_element = driver.find_element(By.CSS_SELECTOR, 'label[for="check645num23"]')
+    expected_nums = [1, 2, 3, 4, 5, 6]  # 예시 리스트, 실제 사용할 값으로 변경
 
-    # 라벨의 텍스트 변경하기
-    new_text = "23"
-    driver.execute_script(f"arguments[0].innerText = '{new_text}';", label_element)
-    time.sleep(2)
-    # 라벨의 텍스트가 변경되었는지 검증하기
-    updated_label_element = driver.find_element(By.CSS_SELECTOR, 'label[for="check645num23"]')
-    print(updated_label_element.text)
+    # 예상 번호들을 선택(페이지에서 클릭)
+    for num in expected_nums:
+        element_id = f"check645num{num}"
+        driver.execute_script(f"document.getElementById('{element_id}').click();")
+        time.sleep(1)  # 페이지 업데이트를 기다리기 위해 추가
+    # 확인 버튼을 클릭
+    driver.find_element(By.ID, 'btnSelectNum').click()
 
 finally:
     print("end")
