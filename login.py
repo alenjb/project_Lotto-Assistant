6db857d42792fae081734a63838dccd7a16bfedf
alenjb <alenjb@naver.com>
2024-01-11 22:28:10 +0900

    ✨feat(login):동행복권 구매 페이지(iframe)에서 확인 버튼을 눌러 구매를 완료하는 기능 구현
    - 예상 번호 리스트들을 가져와 동행복권 구매 페이지(iframe)에서 확인 버튼을 눌러 구매를 완료하는 기능 구현
    - 총 구매 금액 출력 구현

--- login.py
+++ login.py
@@ -13,35 +13,47 @@ import account
 # webdriver.Chrome 대신 Chrome으로 수정
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-try:
-    # 웹페이지 열기
-    driver.get("https://dhlottery.co.kr/user.do?method=login&returnUrl=")
 
-    # 입력 필드에 값 입력
-    user_id = account.user_id
-    password = account.password
+def login_and_buy(expected_nums_tuple):
+    try:
+        # 웹페이지 열기
+        driver.get("https://dhlottery.co.kr/user.do?method=login&returnUrl=")
 
-    driver.find_element(By.ID, "userId").send_keys(user_id)
-    driver.find_element(By.NAME, "password").send_keys(password)
+        # 입력 필드에 값 입력
+        user_id = account.user_id
+        password = account.password
 
-    # 로그인 버튼 클릭
-    driver.find_element(By.CSS_SELECTOR, 'a.btn_common.lrg.blu').click()
-    driver.get("https://el.dhlottery.co.kr/game/TotalGame.jsp?LottoId=LO40")
+        driver.find_element(By.ID, "userId").send_keys(user_id)
+        driver.find_element(By.NAME, "password").send_keys(password)
 
-    # 대기 시간을 10초로 설정하고 iframe이 나타날 때까지 기다립니다.
-    WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.ID, "ifrm_tab")))
+        # 로그인 버튼 클릭
+        driver.find_element(By.CSS_SELECTOR, 'a.btn_common.lrg.blu').click()
+        driver.get("https://el.dhlottery.co.kr/game/TotalGame.jsp?LottoId=LO40")
 
-    expected_nums = [1, 2, 3, 4, 5, 6]  # 예시 리스트, 실제 사용할 값으로 변경
+        # 대기 시간을 10초로 설정하고 iframe이 나타날 때까지 기다립니다.
+        WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.ID, "ifrm_tab")))
 
-    # 예상 번호들을 선택(페이지에서 클릭)
-    for num in expected_nums:
-        element_id = f"check645num{num}"
-        driver.execute_script(f"document.getElementById('{element_id}').click();")
-        time.sleep(1)  # 페이지 업데이트를 기다리기 위해 추가
-    # 확인 버튼을 클릭
-    driver.find_element(By.ID, 'btnSelectNum').click()
+        for expected_nums in expected_nums_tuple:
+            # 예상 번호들을 선택(페이지에서 클릭)
+            for num in expected_nums:
+                element_id = f"check645num{num}"
+                driver.execute_script(f"document.getElementById('{element_id}').click();")
+                time.sleep(0.1)  # 페이지 업데이트를 기다리기 위해 추가
 
-finally:
-    print("end")
-    # 크롬 창 닫기
-    # driver.quit()
+            # 확인 버튼을 클릭
+            driver.find_element(By.ID, 'btnSelectNum').click()
+
+
+        # 구매하기 버튼을 클릭
+        driver.find_element(By.ID, 'btnBuy').click()
+
+        # 확인 버튼 클릭
+        # JavaScript로 확인 버튼 클릭
+        js_confirm_code = """document.querySelector("#popupLayerConfirm > div > div.btns > input:nth-child(1)").click();"""
+        js_exit_code = """document.querySelector("#closeLayer").click();"""
+        driver.execute_script(js_confirm_code)
+        driver.execute_script(js_exit_code)
+
+    finally:
+        # 크롬 창 닫기
+        driver.quit()
--- lotto_generator.py
+++ lotto_generator.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 
 import control_num
 import get_num
+import login
 
 # 현재 회차 번호 가져오기
 cur_num = get_num.get_current_draw_num()
@@ -13,53 +14,63 @@ last_week, two_weeks_ago = get_num.get_last_two_weeks_numbers(cur_num)
 
 
 # 로또 번호 생성 함수
-def lotto_generator():
-    lotto_numbers = []
-    # 지난 회차 번호와 겹치는 개수
-    last_duplicated = 0
-    # 지지난 회차 번호와 겹치는 개수
-    two_ago_duplicated = 0
+def lotto_generator(sets_unmber):
+    result_lists = []
+    # 구매 세트 수만큼 반복
+    for _ in range(sets_unmber):
+        lotto_numbers = []
+        # 지난 회차 번호와 겹치는 개수
+        last_duplicated = 0
+        # 지지난 회차 번호와 겹치는 개수
+        two_ago_duplicated = 0
 
-    # 생성가능한 숫자들
-    capable_numbers = list(range(1, 46))
-    while len(lotto_numbers) < 6:
-        # 생성가능한 숫자들에서 로또 번호 추출
-        number = random.choice(capable_numbers)
-        last_duplicated, two_ago_duplicated = control_num.has_same_with_last_week(number, last_week, two_weeks_ago, last_duplicated, two_ago_duplicated)
-        # 조건 1: 지난 주와 지지난 주 회차와 같은 숫자가 3개 이상 존재하지 않는다.
-        if last_duplicated < 3 and two_ago_duplicated < 3:
-            # 조건 2: 3개 이상의 숫자가 연속되어 나오지 않는다.
-            if control_num.has_consecutive_numbers_more_than_three(lotto_numbers) is False:
-                # 조건 3: 추첨 숫자 중 같은 10의 자리 숫자가 3개 이상 존재하지 않는다.
-                if control_num.has_same_three_tens(lotto_numbers) is False:
-                    # 추출 번호를 리스트에 추가
-                    lotto_numbers.append(number)
-                    capable_numbers.remove(number)
-                # 지난 주와 지지난 주의 당첨번호와 3개 이상 겹치지 않고 3개 이상의 숫자가 연속되지 않지만 같은 10번대의 숫자가 3개인 경우
+        # 생성가능한 숫자들
+        capable_numbers = list(range(1, 46))
+        while len(lotto_numbers) < 6:
+            # 생성가능한 숫자들에서 로또 번호 추출
+            number = random.choice(capable_numbers)
+            last_duplicated, two_ago_duplicated = control_num.has_same_with_last_week(number, last_week, two_weeks_ago, last_duplicated, two_ago_duplicated)
+            # 조건 1: 지난 주와 지지난 주 회차와 같은 숫자가 3개 이상 존재하지 않는다.
+            if last_duplicated < 3 and two_ago_duplicated < 3:
+                # 조건 2: 3개 이상의 숫자가 연속되어 나오지 않는다.
+                if control_num.has_consecutive_numbers_more_than_three(lotto_numbers) is False:
+                    # 조건 3: 추첨 숫자 중 같은 10의 자리 숫자가 3개 이상 존재하지 않는다.
+                    if control_num.has_same_three_tens(lotto_numbers) is False:
+                        # 추출 번호를 리스트에 추가
+                        lotto_numbers.append(number)
+                        capable_numbers.remove(number)
+                    # 지난 주와 지지난 주의 당첨번호와 3개 이상 겹치지 않고 3개 이상의 숫자가 연속되지 않지만 같은 10번대의 숫자가 3개인 경우
+                    else:
+                        continue
+                # 지난 주와 지지난 주의 당첨번호와 3개 이상 겹치지 않지만 3개 이상의 숫자가 연속된 경우
                 else:
                     continue
-            # 지난 주와 지지난 주의 당첨번호와 3개 이상 겹치지 않지만 3개 이상의 숫자가 연속된 경우
+            # 지난 주와 지지난 주의 당첨번호와 3개 이상 겹치는 경우
             else:
+                # 지난 주 번호와 겹친 경우
+                if last_duplicated == 3:
+                    last_duplicated -= 1
+                    capable_numbers.remove(number)
+                # 지지난 주 번호와 겹친 경우
+                if two_ago_duplicated == 3:
+                    two_ago_duplicated -= 1
+                    capable_numbers.remove(number)
                 continue
-        # 지난 주와 지지난 주의 당첨번호와 3개 이상 겹치는 경우
-        else:
-            # 지난 주 번호와 겹친 경우
-            if last_duplicated == 3:
-                last_duplicated -= 1
-                capable_numbers.remove(number)
-            # 지지난 주 번호와 겹친 경우
-            if two_ago_duplicated == 3:
-                two_ago_duplicated -= 1
-                capable_numbers.remove(number)
-            continue
-    return lotto_numbers
+        result_lists.append(lotto_numbers)
+    return result_lists
 
 
 # 로또 번호 생성 및 출력
+# 구매할 세트 수를 입력받기
+sets_number = int(input("구매할 세트 수를 입력하세요: "))
+buy_amount = 1000 * sets_number
+expected_nums = sorted(lotto_generator(sets_number))
+print("번호 추출이 완료되었습니다.")
+login.login_and_buy(expected_nums)
+# 결과를 출력
 print("-------------------- 제", cur_num, "회 로또 예상 번호 --------------------")
-for j in range(1, 3):
-    print(j, "번째 로또 번호:", sorted(lotto_generator()))
-
-
+for i, j in zip(range(1, len(expected_nums)+1), expected_nums):
+    print(i, "번째 로또 번호:", j)
+print("총 ", format(buy_amount, ','), "원어치 로또를 구매를 완료하였습니다.")
 
 
